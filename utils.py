c17d57bf5eceb6b846ff6f36e0fae215c06511b9
birdenly <jomacaldas@hotmail.com.br>
2025-08-23 23:42:55 -0300

    improvemments and Main folder backup function done

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import math
 import os
-import time
+import shutil
 import zipfile
 
-OUTPUT = os.path.join(os.getcwd(),"backup") 
+OUTPUTMAIN = os.path.join(os.getcwd(),"backup_main.zip")
+OUTPUTENV = os.path.join(os.getcwd(),"backup_env.zip")
 
 def convert_size(size_bytes):
     if size_bytes == 0:
@@ -14,7 +15,7 @@ def convert_size(size_bytes):
     s = round(size_bytes / p, 2)
     return "%s %s" % (s, size_name[i])
 
-def get_size(path='.'):
+def get_size(path):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(path):
         for f in filenames:
@@ -22,34 +23,70 @@ def get_size(path='.'):
             total_size += os.path.getsize(fp)
     return total_size
 
-def ncEnvBackup(path):
-    print("Backing up...")
-    if os.path.exists(OUTPUT + ".zip"):
-        os.remove(OUTPUT + ".zip")
-        print("Old backup removed.")
+def backupExist(output):
+    if os.path.exists(output):
+        print("\n")
+        print("Backup already exists, do you want to remove it? (y/n)")
+        inputChoice = input().lower()
+        if inputChoice == 'y':
+            os.remove(output)
+            print("Old backup removed.")
+            return False
+        else:
+            shutil.move(output, output + ".bak")
+            print(f"Old backup has been changed to {output}.bak")
+            return True
+    else:
+        return False
         
-    with zipfile.ZipFile(OUTPUT + ".zip", "w", strict_timestamps=False) as zf:
+        
+
+def ncEnvBackup(path):
+    print("Backing up environment folder...")
+    backupExist(OUTPUTENV)
+    
+    with zipfile.ZipFile(OUTPUTENV, "w", strict_timestamps=False) as zf:
         for dirpath, dirnames, filenames in os.walk(path):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                zf.write(fp, os.path.relpath(fp, path))
-    print(f"Backup created at {OUTPUT}.zip")
+            for file in filenames:
+                final = os.path.join(dirpath, file)
+                finalPath = os.path.join("NucleusCoop", os.path.relpath(final, path))
+                zf.write(final, finalPath)
+    print(f"Backup created at {OUTPUTENV}")
 
         
 def ncMainBackup(path):
-    size = convert_size(get_size(path))
-    print(f"Nucleus main folder path: {path}")
-    print(f"Folder size:  {size}")
-    accept = input("Do you want to backup Nucleus Enviromment folder? (y/n): ")
-    if accept.lower() == 'y':
-        print("Backing up...")
-        if os.path.exists(OUTPUT + ".zip"):
-            os.remove(OUTPUT + ".zip")
-            print("Old backup removed.")
-            
-        with zipfile.ZipFile(OUTPUT + ".zip", "w", strict_timestamps=False) as zf:
-            for dirpath, dirnames, filenames in os.walk(path):
-                for f in filenames:
-                    fp = os.path.join(dirpath, f)
-                    zf.write(fp, os.path.relpath(fp, path))
-        print(f"Backup created at {OUTPUT}.zip")
+    print("Backing up main folder...")
+    backupExist(OUTPUTMAIN)
+    with zipfile.ZipFile(OUTPUTMAIN, "w", strict_timestamps=False) as zf:
+        for dirpath, dirnames, filenames in os.walk(path): #goes through all files and folders
+            for file in filenames:
+                #always get settings.ini
+                if file == "Settings.ini": 
+                    final = os.path.join(dirpath, file)
+                    zf.write(final, os.path.relpath(final, path))
+                
+                #only get the main ones
+                if os.path.basename(dirpath) in ["game profiles", "Profiles"]:
+                    root, extension = os.path.splitext(os.path.join(dirpath, file))
+                    if os.path.islink(os.path.join(dirpath, file)):
+                        continue
+                    if extension in [".exe", ".log"]:
+                        continue
+                    if file.startswith("steam_api") and extension == ".dll":
+                        continue
+                    final = os.path.join(dirpath, file)
+                    zf.write(final, os.path.relpath(final, path))
+                
+                #logic to get the content folder and the subdirs
+                sub = os.path.join(dirpath, file)
+                parent = os.path.join(path, "content")
+                if sub.startswith(parent):
+                    root, extension = os.path.splitext(os.path.join(dirpath, file))
+                    if os.path.islink(os.path.join(dirpath, file)):
+                        continue
+                    if extension in [".exe", ".log"]:
+                        continue
+                    final = os.path.join(dirpath, file)
+                    zf.write(final, os.path.relpath(final, path))
+                
+    print(f"Backup created at {OUTPUTMAIN}")
